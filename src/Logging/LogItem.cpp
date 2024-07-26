512ddf000342f543f337fcd434f8dfc8348b7787
Siddharth Mohanty <neosiddharth@gmail.com>
2024-07-25 17:43:25 -0700

    del: spinoff logging into its own project

--- src/Logging/LogItem.cpp
+++
@@ -1,9 +0,0 @@
-#include "LogItem.hpp"
-
-Logging::LogItem::~LogItem() {}
-
-Logging::LogItem::LogItem(std::string source, std::string message) {
-    this->time = std::chrono::system_clock::now();
-    this->source = source;
-    this->message = message;
-}
