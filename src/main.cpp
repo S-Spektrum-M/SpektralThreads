8ec6a76c5aa0c390b574aa0a5a8a882acf85340c
Siddharth Mohanty <neosiddharth@gmail.com>
2024-07-25 17:41:28 -0700

    fix: default

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,2 @@
-#include <iostream>
-using std::cout;
-using std::endl;
-#include "Logging/Log.hpp"
-#include "Logging/LogItem.hpp"
-
 int main() {
-    Logging::Log * lg = Logging::Log::getInstance();
-    cout << &(lg->root->message) << endl;
-    lg->insertItem("main", "TEST");
-    lg->insertItem("main", "TEST");
-    lg->insertItem("main", "TEST");
-    lg->insertItem("main", "TEST");
-    lg->insertItem("main", "TEST");
 }
