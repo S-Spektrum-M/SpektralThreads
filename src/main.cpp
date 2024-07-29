9c0b5fcf54ce1340ffe69532e6a84dde4104c9c4
Siddharth Mohanty <neosiddharth@gmail.com>
2024-07-28 23:47:31 -0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 int main() {
     // Sample usage of thread pool
+    // Hello ths is a test
     auto pool = Threading::Pool::getInst();
     pool->Start(2);
     Threading::Job example = [](std::shared_ptr<void> arg){
